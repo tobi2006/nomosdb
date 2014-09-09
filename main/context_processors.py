df42a1caf0270dc5a9e38783826b3eb293c54fa4
Tobias Kliem <tobias.kliem@gmail.com>
2014-09-09 09:10:57 +0100

    Added student categories to context processor with proper tests

--- main/context_processors.py
+++ main/context_processors.py
@@ -23,22 +23,19 @@ def menubar(request):
         except Settings.DoesNotExist:
             current_year = int(START_YEAR)
             Settings.objects.create(name="current_year", value=current_year)
+        staff_subject_areas = staff.subject_areas.all().values('name')
         if staff.role == 'teacher':
             if staff.programme_director:
-                teacher_subject_areas = staff.subject_areas.all().values(
-                    'name')
                 modules = Module.objects.filter(
-                    subject_areas__name__in=teacher_subject_areas)
+                    subject_areas__name__in=staff_subject_areas)
             else:
                 modules = staff.modules.all()
         else:
             if staff.main_admin:
                 modules = Module.objects.all()
             else:
-                admin_subject_areas = staff.subject_areas.all().values(
-                    'name')
                 modules = Module.objects.filter(
-                    subject_areas__name__in=admin_subject_areas)
+                    subject_areas__name__in=staff_subject_areas)
         for module in modules:
             if module.year == current_year:
                 current.append(module)
@@ -53,8 +50,37 @@ def menubar(request):
         module_dict['current'] = current
         module_dict['past'] = past
         module_dict['future'] = future
+
+        student_list = []
+        other_categories = []
+        if staff.main_admin:
+            relevant_students = Student.objects.all()
+        else:
+            relevant_students = Student.objects.filter(
+                course__subject_areas__name__in=staff_subject_areas)
+        if relevant_students.filter(year=1, active=True).exists():
+            student_list.append(('1', 'Year 1'))
+        if relevant_students.filter(year=2, active=True).exists():
+            student_list.append(('2', 'Year 2'))
+        if relevant_students.filter(year=3, active=True).exists():
+            student_list.append(('3', 'Year 3'))
+        if relevant_students.filter(year=7, active=True).exists():
+            student_list.append(('7', 'Masters Students'))
+        if relevant_students.filter(year=8, active=True).exists():
+            student_list.append(('8', 'PhD Students'))
+        if relevant_students.filter(year=9, active=True).exists():
+            student_list.append(('9', 'Alumni'))
+        if relevant_students.filter(year=None, active=True).exists():
+            other_categories.append(
+                ('unassigned', 'Students not assigned to a Year')
+            )
+        if relevant_students.filter(active=False).exists():
+            other_categories.append(('inactive', 'Inactive Students'))
+
     else:
-        module_dict = {}
+        module_dict = None
     return {
-        'module_dict': module_dict
+        'module_dict': module_dict,
+        'menu_student_categories': student_list,
+        'menu_other_categories': other_categories
     }
--- main/tests/test_context_processors.py
+++ main/tests/test_context_processors.py
@@ -51,6 +51,50 @@ class TeacherMenubarTest(TeacherUnitTest):
         self.assertTrue(module2.get_absolute_url() in modules)
         self.assertFalse(module3.get_absolute_url() in modules)
 
+    def test_teachers_see_students_categories_for_their_areas(self):
+        stuff = set_up_stuff()
+        subject_area = create_subject_area()
+        course = Course.objects.create(title="BA in Cartoon Studies")
+        course.subject_areas.add(subject_area)
+        self.user.staff.subject_areas.add(subject_area)
+        student1 = stuff[1]
+        student1.course = course
+        student1.year = 1
+        student1.save()
+        student2 = stuff[2]
+        student2.course = course
+        student2.year = 2
+        student2.save()
+        student3 = stuff[3]
+        student3.year = 3  # Not in teachers' subject areas
+        student3.save()
+        student4 = stuff[4]
+        student4.course = course
+        student4.year = 8
+        student4.save()
+        student5 = stuff[5]
+        student5.year = 9
+        student5.course = course
+        student5.active = False
+        student5.save()
+        request = self.factory.get('/')
+        request.user = self.user
+        response = home(request)
+        soup = BeautifulSoup(response.content)
+        student_categories = str(soup.select('#menu-student-list'))
+        students = Student.objects.all()
+        self.assertTrue('Year 1' in student_categories)
+        self.assertTrue('<a href="/students/1/">' in student_categories)
+        self.assertTrue('Year 2' in student_categories)
+        self.assertTrue('<a href="/students/2/">' in student_categories)
+        self.assertFalse('Year 3' in student_categories)
+        self.assertFalse('<a href="/students/3/">' in student_categories)
+        self.assertTrue('PhD' in student_categories)
+        self.assertTrue('<a href="/students/8/">' in student_categories)
+        self.assertFalse('Alumni' in student_categories)
+        self.assertTrue('Inactive Students' in student_categories)
+        self.assertTrue('<a href="/students/inactive/">' in student_categories)
+
 class AdminMenubarTest(AdminUnitTest):
     """Tests if the right models are contained in the teacher's menubar"""
 
@@ -109,3 +153,49 @@ class AdminMenubarTest(AdminUnitTest):
         self.assertTrue(module1.get_absolute_url() in modules)
         self.assertTrue(module2.get_absolute_url() in modules)
         self.assertTrue(module3.get_absolute_url() in modules)
+
+    def test_main_admin_sees_all_student_categories(self):
+        stuff = set_up_stuff()
+        subject_area = create_subject_area()
+        course = Course.objects.create(title="BA in Cartoon Studies")
+        course.subject_areas.add(subject_area)
+        self.user.staff.subject_areas.add(subject_area)
+        self.user.staff.main_admin = True
+        self.user.staff.save()
+        student1 = stuff[1]
+        student1.course = course
+        student1.year = 1
+        student1.save()
+        student2 = stuff[2]
+        student2.course = course
+        student2.year = 2
+        student2.save()
+        student3 = stuff[3]
+        student3.course = course
+        student3.year = 7
+        student3.save()
+        student4 = stuff[4]
+        student4.course = course
+        student4.year = 9
+        student4.save()
+        student5 = stuff[5]
+        student5.year = 1
+        student5.active = False
+        student5.save()
+        request = self.factory.get('/')
+        request.user = self.user
+        response = home(request)
+        soup = BeautifulSoup(response.content)
+        student_categories = str(soup.select('#menu-student-list'))
+        self.assertTrue('Year 1' in student_categories)
+        self.assertTrue('<a href="/students/1/">' in student_categories)
+        self.assertTrue('Year 2' in student_categories)
+        self.assertTrue('<a href="/students/2/">' in student_categories)
+        self.assertFalse('Year 3' in student_categories)
+        self.assertFalse('<a href="/students/3/">' in student_categories)
+        self.assertTrue('Masters' in student_categories)
+        self.assertTrue('<a href="/students/7/">' in student_categories)
+        self.assertTrue('Alumni' in student_categories)
+        self.assertTrue('<a href="/students/9/">' in student_categories)
+        self.assertTrue('Inactive Students' in student_categories)
+        self.assertTrue('<a href="/students/inactive/">' in student_categories)
