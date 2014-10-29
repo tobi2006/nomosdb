d90414cd93139a780dec97e2b3dc303f376bc193
Tobias Kliem <tobias.kliem@gmail.com>
2014-10-29 21:47:41 +0000

    Added test for Legal Problem

--- feedback/tests/test_forms.py
+++ feedback/tests/test_forms.py
@@ -38,6 +38,23 @@ class IndividualFeedbackFormTests(TeacherUnitTest):
         self.assertIn('id="id_comments"', form.as_p())
         self.assertIn('id="id_mark"', form.as_p())
 
+    def test_form_is_generated_correctly_for_legal_problem(self):
+        IndividualFeedbackForm = get_individual_feedback_form('LEGAL_PROBLEM')
+        form = IndividualFeedbackForm()
+        self.assertIn('id="id_markers"', form.as_p())
+        self.assertIn('id="id_marking_date"', form.as_p())
+        self.assertIn('id="id_submission_date"', form.as_p())
+        self.assertIn('id="id_category_mark_1"', form.as_p())
+        self.assertIn('id="id_category_mark_2"', form.as_p())
+        self.assertIn('id="id_category_mark_3"', form.as_p())
+        self.assertIn('id="id_category_mark_4"', form.as_p())
+        self.assertNotIn('id="id_category_mark_5"', form.as_p())
+        self.assertNotIn('id="id_category_mark_6"', form.as_p())
+        self.assertNotIn('id="id_category_mark_7"', form.as_p())
+        self.assertNotIn('id="id_category_mark_8"', form.as_p())
+        self.assertIn('id="id_comments"', form.as_p())
+        self.assertIn('id="id_mark"', form.as_p())
+
     def test_form_validates_mark(self):
         IndividualFeedbackForm = get_individual_feedback_form('ESSAY')
         form1 = IndividualFeedbackForm(data={'mark': 'Not a number'})
