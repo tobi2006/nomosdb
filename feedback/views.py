f270df3b6df5ab580e7bc55e3c84c673edf17d0f
Tobias Kliem <tobias.kliem@gmail.com>
2015-09-23 23:43:46 +0100

    Added additional sentence for feedback to forms, now need to add it to export

--- feedback/views.py
+++ feedback/views.py
@@ -69,7 +69,8 @@ def individual_feedback(
     else:
         resit = True
         marksheet_type = assessment.resit_marksheet_type
-    IndividualFeedbackForm = get_individual_feedback_form(marksheet_type)
+    IndividualFeedbackForm = get_individual_feedback_form(
+        marksheet_type, assessment.co_marking)
     if request.method == 'POST':
         form = IndividualFeedbackForm(instance=feedback, data=request.POST)
         if form.is_valid():
@@ -159,7 +160,7 @@ def group_feedback(
     else:
         marksheet_type = assessment.resit_marksheet_type
     IndividualFeedbackForm = get_individual_feedback_form_for_group(
-        marksheet_type)
+        marksheet_type, assessment.co_marking)
     GroupFeedbackForm = get_group_feedback_form(marksheet_type)
     marksheet_type = CATEGORIES[assessment.marksheet_type]
     split = marksheet_type['split']
